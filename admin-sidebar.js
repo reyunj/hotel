c11d621ebe7cf30116b6471bf1d814e2a5532978
reyunj <junrey@agentfinance.ai>
2026-05-08 16:23:39 +0800

    Replace SVG logo with hotel image in admin sidebar and relocate logo file to public directory

--- admin-sidebar.js
+++ admin-sidebar.js
@@ -8,10 +8,7 @@ function loadSidebar(activePage) {
     <aside class="sidebar" id="sidebar">
         <div class="sidebar-header">
             <a href="admin.html" class="sidebar-logo" onclick="closeSidebarOnMobile(event)">
-                <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/>
-                    <polyline points="9,22 9,12 15,12 15,22"/>
-                </svg>
+                <img src="public/hotelLogo.jpg" alt="Tagboanan Resort" class="sidebar-logo-img">
                 Tagboanan Resort
             </a>
         </div>
